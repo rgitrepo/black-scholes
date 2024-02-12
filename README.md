1b2e502dd965eca931d952870097f0359745c13a
Raja Khan <rigitrepo@users.noreply.github.com>
2024-02-12 10:11:32 -0500

    updated readme and added comments in code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # black-scholes
-Black scholes implementation in Python
+Black-Scholes-Merton model implementation in Python
+
+This model calculates the theoretical value of an option based on five parameters:
+
+Underlying Price (S):   The current market price of the asset.
+Strike Price (K):       The predetermined price at which the option can be exercised.
+Time to Expiration (T): The time left (in years) until the option’s expiration date.
+Risk Free Rate (r):     The constant rate of return on a risk-free asset, such as a government bond.
+Volatility (σ):         A measure of how much the price of the underlying asset fluctuates.
+
--- black-scholes.py
+++ black-scholes.py
@@ -1,3 +1,14 @@
+
+
+"""
+Underlying Price (S):   The current market price of the asset.
+Strike Price (K):       The predetermined price at which the option can be exercised.
+Time to Expiration (T): The time left (in years) until the option’s expiration date.
+Risk Free Rate (r):     The constant rate of return on a risk-free asset, such as a government bond.
+Volatility (σ):         A measure of how much the price of the underlying asset fluctuates.
+"""
+
+
 import math
 from scipy.stats import norm
 
@@ -11,12 +22,13 @@ d1 = (math.log(S/K) + (r + 0.5 * vol**2)*T ) / (vol * math.sqrt(T))
 
 d2 = d1 - (vol * math.sqrt(T))
 
-# Call
+# Call Option Price calculation
 C = S * norm.cdf(d1) - K * math.exp(-r * T) * norm.cdf(d2)
 
-# Put
+# Put Option Price calculation
 P = K * math.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
 
+# Print the results
 print('The value of d1 is: ', round(d1, 4))
 print('The value of d2 is: ', round(d2, 4))
 print('The price of the call option is: $', round(C, 2))
